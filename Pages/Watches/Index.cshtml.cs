e9b7b9a7b6a8e48fba7d10bee1ef64ab7c76582c
timocheu <ccs.timotheoche@gmail.com>
2026-01-26 13:44:52 +0800

    Add search box and brand filter

--- Pages/Watches/Index.cshtml.cs
+++ Pages/Watches/Index.cshtml.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 using RazorPageWatches.Data;
 using RazorPageWatches.Models;
 
@@ -21,9 +23,35 @@ namespace RazorPageWatches.Pages.Watches
 
         public IList<Watch> Watch { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchModel { get; set; }
+
+        public SelectList? Brand { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? WatchBrand { get; set; }
+
         public async Task OnGetAsync()
         {
-            Watch = await _context.Watch.ToListAsync();
+            IQueryable<string> brandQuery = from m in _context.Watch
+                                            orderby m.Brand
+                                            select m.Brand;
+
+            var watches = from m in _context.Watch select m;
+
+            if (!string.IsNullOrEmpty(WatchBrand))
+            {
+                watches = watches.Where(x => x.Brand == WatchBrand);
+            }
+
+            if (!string.IsNullOrEmpty(SearchModel))
+            {
+                watches = watches.Where(s => s.Model.Contains(SearchModel));
+            }
+
+            Brand = new SelectList(await brandQuery.Distinct().ToListAsync());
+
+            Watch = await watches.ToListAsync();
         }
     }
 }
